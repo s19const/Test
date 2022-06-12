8ce0a73d2e3c64e95ffb9b6eeb33be8b28d450a5
const-test <kvshvarov@yandex.ru>
2022-06-12 23:35:50 +0300

    test

--- src/test.java
+++ src/test.java
@@ -1,6 +1,8 @@
-public class test {
-    public static void test(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         System.out.println("УПС");
+
+        System.out.println("УПС+2");
 
     }
 }
